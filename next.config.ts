d85b9f40bcfdb3972502d4b8418f7d001ea7f37b
Krishna Kumar <krishna.kumar@logicvalley.in>
2026-02-02 17:53:31 +0530

    Remove export script in pipeline yml file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 module.exports = {
+  output: "export",
+  images: { unoptimized: true },
   transpilePackages: [
     "react-web-white-label",
     "react-dnd",
     "react-dnd-html5-backend",
     "prismjs",
   ],
-  output: "export",
-  images: { unoptimized: true },
 };
